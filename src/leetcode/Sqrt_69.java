0d6bdbeafbd7da356815cab7a795c36fc0b1e5a9
Administrator <chen405711882@gmail.com>
2023-10-02 23:18:05 +1100

    Update

---
+++ src/leetcode/Sqrt_69.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+public class Sqrt_69 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int test(int x){
+        if (x < 2) return x;
+        long num;
+        int pivot, left = 2, right = x / 2;
+        while (left <= right) {
+            pivot = left + (right - left) / 2;
+            num = (long)pivot * pivot;
+            if (num > x) right = pivot - 1;
+            else if (num < x) left = pivot + 1;
+            else return pivot;
+        }
+        return right;
+    }
+    public static int test1(int x) {
+        if (x<2) {
+            return x;
+        }
+        long num;
+        int pivot, left = 2, right = x / 2;
+        while (left <= right) {
+            pivot = left + (right - left) / 2;
+            num = (long) pivot * pivot;
+            if (num > x) {
+                right = pivot - 1;
+            }
+            else if (num < x) {
+                left = pivot + 1;
+            }
+            return pivot;
+        }
+        return right;
+    }
+}
