90cdf7af3cd5b310a6f05d49daf9547b23bfae27
Administrator <chen405711882@gmail.com>
2023-10-02 23:16:43 +1100

    Update

---
+++ src/leetcode/ValidAnagram_242.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class ValidAnagram_242 {
+    public static void main(String[] args) {
+        String s = "unkonw";
+        String t = "unkonw";
+        int [] a = new int [26];
+        for(char i:s.toCharArray()) {
+            a[i-'a']++;
+        }
+        for(char j:t.toCharArray()) {
+            a[j-'a']--;
+
+        }
+        for(int i:a) {
+            if(i!=0) {
+                System.out.println("yes");
+            }
+        }
+        System.out.println("no");
+    }
+
+}
